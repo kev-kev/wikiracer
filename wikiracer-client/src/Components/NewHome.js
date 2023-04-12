afe364024f80a490b3fc3f805ee73663f8fcb20d
kev-kev <kevin.clark93@gmail.com>
2023-04-12 15:33:47 -0400

    Add and style initial home grid

--- wikiracer-client/src/Components/NewHome.js
+++ wikiracer-client/src/Components/NewHome.js
@@ -4,10 +4,8 @@ const NewHome = () => {
   return (
     <div>
       <div className='home-grid'>
-        <div className='welcome-logo-container'>
-          <h2 className='welcome'>Welcome to</h2>
-          <img src="logo.png" alt="logo"/>
-        </div>
+        <h2 className='welcome'>Welcome to</h2>
+        <img className="logo" src="logo.png" alt="logo"/>
         <h1 className='title'>WikiRacer</h1>
       </div>
     </div>
