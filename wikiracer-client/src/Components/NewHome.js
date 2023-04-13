826979586076e99dff971512ac7118bde8e2d643
kev-kev <kevin.clark93@gmail.com>
2023-04-13 12:21:56 -0400

    Changing home layout to flex

--- wikiracer-client/src/Components/NewHome.js
+++ wikiracer-client/src/Components/NewHome.js
@@ -4,11 +4,11 @@ const NewHome = () => {
   return (
     <div className="home">
       <div className="home-container">
-        <div className='home-grid'>
-          <h2 className='welcome'>Welcome to</h2>
+        <div className="welcome-container">
           <img className="logo" src="logo.png" alt="logo"/>
-          <h1 className='title'>WikiRacer</h1>
+          <h2 className='welcome'>Welcome to</h2>
         </div>
+          <h1 className='title'>WikiRacer</h1>
         <img className="car" src="car-vroom.png" alt="car going vroom"/>
         <div className='home-btn-container'>
           <button className="join-room-btn home-btn">JOIN</button>
